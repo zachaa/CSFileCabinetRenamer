edfaf3cdac817b4a8472a4c79278be3f499d5da4
zachaa <57045291+zachaa@users.noreply.github.com>
2022-02-18 13:25:48 -0600

    Updated File Class to use new TextInformation while cleaning up TextConvert

--- CSFileCabinetRenamer/File.cs
+++ CSFileCabinetRenamer/File.cs
@@ -1,43 +1,126 @@
-﻿using System.Diagnostics;
-using System.IO;
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Documents;
+using CSFileCabinetRenamer.TextInfo;
+using System.Collections.Generic;
 
 namespace CSFileCabinetRenamer
 {
     public class File
     {
-        public string FileName { get; set; }
-        public string FileExtension { get; set; }
-        public string FilePath { get; set; }
-        public bool IsCleanName { get; set; }
-        public Brush IsCleanBackground { get; set; }
+        public string FileName { get; }
+        public string FileExtension { get; }
+        public string FilePath { get; }
 
+        public bool IsCleanName { get; }
+        public Brush BackgroundBrush { get; set; }
+        /// <summary> Highlighted Text to put in the GridView </summary>
+        // public FlowDocument FileNameFlowDocument { get; set; }
+        public List<Inline> FileNameInlineList { get; set; }
+
+        private TextElementsHolder textElementsHolder {get;}
+        private bool _HasCombiningChar { get; set; }
+        private bool _HasExtendedUnicode { get; set; }
+        private bool _HasBeyondUnicode { get; set; }
+
+        // Brushes for IsClean Column
         static readonly LinearGradientBrush _brushGood = new(Color.FromArgb(255, 50, 240, 80), Color.FromArgb(40, 50, 240, 80), 0.0);
+        static readonly LinearGradientBrush _brushFix = new(Color.FromArgb(255, 250, 180, 10), Color.FromArgb(40, 250, 180, 10), 0.0);
         static readonly LinearGradientBrush _brushBad = new(Color.FromArgb(255, 240, 50, 50), Color.FromArgb(40, 240, 50, 50), 0.0);
+        static readonly LinearGradientBrush _brushUnknown = new(Color.FromArgb(255, 100, 100, 100), Color.FromArgb(40, 100, 100, 100), 0.0);
+
+        // Brushes for coloring parts of text to show problems
+        static SolidColorBrush _bCombineChar = new(Color.FromArgb(150, 220, 180, 10));
+        static SolidColorBrush _bExtendedUni = new(Color.FromArgb(150, 220, 40, 40));
+        static SolidColorBrush _bBeyondUni = new(Color.FromArgb(150, 180, 40, 220));
 
         public File(string file_path)
         {
             FileName = Path.GetFileNameWithoutExtension(file_path);
             FileExtension = Path.GetExtension(file_path);
             FilePath = file_path;
-            IsCleanName = IsNameClean();
-            IsCleanBackground = BackgroundBrush();
+
+            textElementsHolder = new TextElementsHolder(FileName);
+            _HasCombiningChar = textElementsHolder.HasCombiningCharacters;
+            _HasExtendedUnicode = textElementsHolder.HasExtendedUnicode;
+            _HasBeyondUnicode = textElementsHolder.HasBeyondUnicode;
+            IsCleanName = textElementsHolder.IsClean();
+
+            BackgroundBrush = BackgroundBrushFromContent();
+            // FileNameFlowDocument = CreateFlowDoc();
+            FileNameInlineList = CreateInlineList();
 
         }
 
-        private bool IsNameClean()
+        /*private FlowDocument CreateFlowDoc()
         {
-            return !TextConveter.HasCombiningCharacters(FileName);
+            var doc = new FlowDocument();
+
+            // Most file name should end be here
+            if (IsCleanName) 
+            { 
+                doc.Blocks.Add(new Paragraph(new Run(FileName)));
+                return doc;
+            }
+
+            var paragraph = new Paragraph();
+            foreach ((int index, TextInfo.TextElement tel) in textElementsHolder.TextElements)
+            {
+                var r = new Run(tel.ToString());
+
+                // Color the background depending on the type of TextElement
+                if (tel.HasBeyondUnicode) { r.Background = _bBeyondUni; }
+                else if (tel.HasExtendedUnicode) { r.Background = _bExtendedUni; }
+                else if (tel.HasCombiningChar) { r.Background = _bCombineChar; }
+                
+                // Add the element to the run to reform the full file name
+                paragraph.Inlines.Add(r);
+            }
+            doc.Blocks.Add(paragraph);
+            return doc;
+        }*/
+
+        private List<Inline> CreateInlineList()
+        {
+            var col = new List<Inline>();
+
+            // Most file name should end be here
+            if (IsCleanName)
+            {
+                col.Add(new Run(FileName));
+                return col;
+            }
+
+            foreach ((int index, TextInfo.TextElement tel) in textElementsHolder.TextElements)
+            {
+                var r = new Run(tel.ToString());
+
+                // Color the background depending on the type of TextElement
+                if (tel.HasBeyondUnicode) { r.Background = _bBeyondUni; }
+                else if (tel.HasExtendedUnicode) { r.Background = _bExtendedUni; }
+                else if (tel.HasCombiningChar) { r.Background = _bCombineChar; }
+
+                // Add the element to the run to reform the full file name
+                col.Add(r);
+            }
+            return col;
         }
 
-        private Brush BackgroundBrush()
+        private Brush BackgroundBrushFromContent()
         {
-            return IsCleanName ? _brushGood : _brushBad ;
+            // Color the IsClean background depending on if it can be fixed or not
+            if (IsCleanName) { return _brushGood; }
+            else if (_HasExtendedUnicode || _HasBeyondUnicode) { return _brushBad; }
+            else if (_HasCombiningChar) { return _brushFix; }
+            else { return _brushUnknown; }
         }
 
         public void ChangeFileName()
         {
-            if (!IsCleanName)
+            // Can't change Extended or Beyond Unicode files, user will have to do that manually
+            if (!IsCleanName && !_HasBeyondUnicode && !_HasExtendedUnicode)
             {
                 if (System.IO.File.Exists(FilePath))
                 {
--- CSFileCabinetRenamer/TextConveter.cs
+++ CSFileCabinetRenamer/TextConveter.cs
@@ -1,37 +1,12 @@
 ﻿using System;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Diagnostics;
 
 namespace CSFileCabinetRenamer
 {
     internal class TextConveter
     {
-        // \u25CC = ◌ just in case that is in there
-        static readonly Regex regexCombineChars = new Regex(@"[\u25CC\u0300-\u036F\u1AB0-\u1AFF\u1DC0-\u1DFF\u20D0-\u20FF\uFE20-\uFE2F]+");
-
-        /// <summary>
-        /// Checks if the given text has any unicode combining characters.
-        /// 
-        /// See: https://en.wikipedia.org/wiki/Combining_character
-        /// 
-        /// Does not check for any language specific combining characters.
-        /// </summary>
-        /// <param name="text">Any text string</param>
-        /// <returns>true if there are combining characters</returns>
-        public static bool HasCombiningCharacters(string text)
-        {
-            MatchCollection combiningChars = regexCombineChars.Matches(text);
-            if (combiningChars.Count > 0) {
-                //Trace.WriteLine("Has Invalid Chars: " + c);
-                //foreach (Match match in combiningChars)
-                //{
-                //    Trace.WriteLine(CharToUnicodeFormat(match.Value));
-                //}
-                return true;
-            }
-            return false;
-        }
+        static readonly Regex regexCombineChars = new Regex(@"[\u0300-\u036F\u1AB0-\u1AFF\u1DC0-\u1DFF\u20D0-\u20FF\uFE20-\uFE2F]+");
 
         /// <summary>
         /// Replace the combining characters in a string.
@@ -40,8 +15,8 @@ namespace CSFileCabinetRenamer
         /// <returns>text string without combining characters</returns>
         public static string RemoveCombiningCharacters(string text)
         {
-            string combiningChars = regexCombineChars.Replace(text, "");
-            return combiningChars;
+            string removedCombineChars = regexCombineChars.Replace(text, "");
+            return removedCombineChars;
         }
 
         /// <summary>
