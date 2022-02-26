701f15e1b8158513fc91dbc661697ff7a70a5331
zachaa <57045291+zachaa@users.noreply.github.com>
2022-02-26 13:52:00 -0600

    Added help window

---
+++ CSFileCabinetRenamer/WindowHelp.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace CSFileCabinetRenamer
+{
+    /// <summary>
+    /// Interaction logic for WindowHelp.xaml
+    /// </summary>
+    public partial class WindowHelp : Window
+    {
+        public WindowHelp()
+        {
+            InitializeComponent();
+        }
+
+        private void okButton_Click(object sender, RoutedEventArgs e) =>
+            DialogResult = true;
+    }
+}
